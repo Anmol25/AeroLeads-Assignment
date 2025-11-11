647454cea0e53d712e7e66aed402d01b83541f7d
Anmol Jindal <anmoljindal025@gmail.com>
2025-11-11 07:14:25 +0000

    updated main.py

--- LinkedIn-Scraping/main.py
+++ LinkedIn-Scraping/main.py
@@ -2,6 +2,6 @@ from src.scrapper import LinkedInScapper
 
 scrapper = LinkedInScapper()
 
-results = scrapper.scrape(["https://www.linkedin.com/in/bushranazir333/"])
+results = scrapper.scrape(["https://www.linkedin.com/in/anmol25/"])
 
 print(results)
